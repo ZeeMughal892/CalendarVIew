fad2e6bbcda1a45a1ae13b89c3d3b92cd6cddaa5
Muhammad Zeeshan <zee.1mughal@gmail.com>
2022-08-09 00:55:51 -0700

    Initial Commit

---
+++ CalendarVIew/CalendarVIewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarVIewApp.swift
+//  CalendarVIew
+//
+//  Created by Muhammad Zeeshan on 2022-08-09.
+//
+
+import SwiftUI
+
+@main
+struct CalendarVIewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
